4734235f77dd5543a87cd64016ce53f70922647e
Michael Rokosh <mike.rokosh@gmail.com>
2015-04-21 13:38:54 +0300

    Initial commit

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'praxie:canvas-to-blob',
-  version: '1.1.0.2',
+  version: '1.1.0',
   summary: 'JavaScript Canvas to Blob is a function to convert canvas elements into Blob objects',
   git: '',
   documentation: 'README.md'
